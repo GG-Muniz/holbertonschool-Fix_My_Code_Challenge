a962cf16921ca41c4424c210d6d9b9680dfecb02
Gabriel Garcia-Muniz <g.garciamuniz@outlook.com>
2025-09-25 17:16:10 -0400

    Reworked to perform deletion using the canonical prev/next rewiring

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -3,36 +3,39 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *node, *to_delete;
-    unsigned int i = 0;
+    dlistint_t *saved, *to_delete;
+    unsigned int i;
 
-    if (!head || !*head)
+    if (head == NULL || *head == NULL)
         return -1;
 
-    node = *head;
     if (index == 0)
     {
-        to_delete = node;
-        *head = node->next;
+        to_delete = *head;
+        *head = (*head)->next;
         if (*head)
             (*head)->prev = NULL;
         free(to_delete);
         return 1;
     }
 
-    while (node && i < index - 1)
-    {
-        node = node->next;
-        i++;
-    }
-    if (!node || !node->next)
-        return -1;
+    saved = *head;
+    for (i = 0; i < index && *head; i++)
+        *head = (*head)->next;
 
-    to_delete = node->next;
-    node->next = to_delete->next;
-    if (to_delete->next)
-        to_delete->next->prev = node;
-    free(to_delete);
+    if (*head == NULL)
+    {
+        *head = saved;
+        return -1;
+    }
+
+    if ((*head)->prev)
+        (*head)->prev->next = (*head)->next;
+    if ((*head)->next)
+        (*head)->next->prev = (*head)->prev;
+
+    free(*head);
+    *head = saved;
     return 1;
 }
 
